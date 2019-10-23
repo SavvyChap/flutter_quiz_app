7bdf1582b9c9ec5604c764b36ac91078f236a8ed
August Money <august@planetexpress.io>
2019-10-23 17:04:04 -0500

    Added in score feature.

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,41 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Blue', 'Red', 'Green', 'Black']
+      'answers': [
+        {'text': 'Blue', 'score': 2},
+        {'text': 'Red', 'score': 4},
+        {'text': 'Green', 'score': 3},
+        {'text': 'Black', 'score': 10}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Cat', 'Dog', 'Lizards', 'Not Fish']
+      'answers': [
+        {'text': 'Cat', 'score': 9},
+        {'text': 'Dog', 'score': 2},
+        {'text': 'Lizards', 'score': 7},
+        {'text': 'Not Fish', 'score': 1}
+       ]
     },
     {
       'questionText': 'What\'s your favorite season?',
-      'answers': ['Spring', 'Summer', 'Fall', 'Winter']
+      'answers': [
+        {'text': 'Spring', 'score': 2},
+        {'text': 'Summer', 'score': 3},
+        {'text': 'Fall', 'score': 2},
+        {'text': 'Winter', 'score': 3}
+      ]
     }
 
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -56,7 +75,7 @@ class _MyAppState extends State<MyApp> {
               answerQuestion: _answerQuestion,
               questionIndex: _questionIndex,
               questions: _questions,)
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'you did it';
+    if (resultScore <= 8) {
+      resultText = 'you are an okay person';
+    } else if (resultScore <= 12) {
+      resultText = 'you are not strange';
+    } else if (resultScore <= 18) {
+      resultText = 'you are naughty';
+    } else {
+      resultText = 'get the fuck out of my kitchen';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
